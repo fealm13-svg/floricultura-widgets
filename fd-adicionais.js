6c276f36236f834e95b5adcd7d3c1350f1025134
fealm13-svg <fe.alm13@gmail.com>
2026-04-21 16:05:58 -0300

    Add files via upload

--- fd-adicionais.js
+++ fd-adicionais.js
@@ -8,52 +8,52 @@
       id: "chocolates",
       label: "Chocolates",
       produtos: [
-        { nome: "Ferrero Rocher 50g (4 un.)", url: "https://www.floriculturadias.com/adicional-caixa-ferrero-rocher-50g-4-unidades", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/272795208/51tuykmesml-_ac_uf1000-1000_ql80-atcgqz9sk1.png", id: "272795208" },
-        { nome: "Ferrero Rocher 100g (8 un.)", url: "https://www.floriculturadias.com/adicional-caixa-ferrero-rocher-100g-8-unidades", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/187411672/d48ec0941b8498eb1d0528a253dca6e1-oxmrvt.jpg", id: "187411672" },
-        { nome: "Ferrero Rocher 150g (12 un.)", url: "https://www.floriculturadias.com/adicional-caixa-ferrero-rocher-150g-12-unidades", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/383449936/remo-o-de-fundo-24-cchgwhmhvc.png", id: "383449936" },
-        { nome: "Lindt LINDOR 37g", url: "https://www.floriculturadias.com/adicional-chocolate-lindt-lindor-37g", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/275194824/224203-800-450-3xwc2wdfq1.png", id: "275194824" },
-        { nome: "Lindt LINDOR 75g", url: "https://www.floriculturadias.com/adicional-chocolate-lindt-lindor-75g", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/275196262/f5cf57d25f596bed9b37f326e1f66072-j68hok5nsv.jpg", id: "275196262" }
+        { nome: "Ferrero Rocher 50g (4 un.)", preco: "28,00", url: "https://www.floriculturadias.com/adicional-caixa-ferrero-rocher-50g-4-unidades", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/272795208/51tuykmesml-_ac_uf1000-1000_ql80-atcgqz9sk1.png" },
+        { nome: "Ferrero Rocher 100g (8 un.)", preco: "45,00", url: "https://www.floriculturadias.com/adicional-caixa-ferrero-rocher-100g-8-unidades", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/187411672/d48ec0941b8498eb1d0528a253dca6e1-oxmrvt.jpg" },
+        { nome: "Ferrero Rocher 150g (12 un.)", preco: "70,00", url: "https://www.floriculturadias.com/adicional-caixa-ferrero-rocher-150g-12-unidades", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/383449936/remo-o-de-fundo-24-cchgwhmhvc.png" },
+        { nome: "Lindt LINDOR 37g", preco: "32,00", url: "https://www.floriculturadias.com/adicional-chocolate-lindt-lindor-37g", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/275194824/224203-800-450-3xwc2wdfq1.png" },
+        { nome: "Lindt LINDOR 75g", preco: "55,00", url: "https://www.floriculturadias.com/adicional-chocolate-lindt-lindor-75g", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/275196262/f5cf57d25f596bed9b37f326e1f66072-j68hok5nsv.jpg" }
       ]
     },
     {
       id: "cartoes",
       label: "Cartões",
       produtos: [
-        { nome: "Cartão de Aniversário 11", url: "https://www.floriculturadias.com/adicional-cartao-de-aniversario-11", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399969300/mockup-cart-es-3-d0hxhh4kbf.png", id: "399969300" },
-        { nome: "Cartão de Amor 8", url: "https://www.floriculturadias.com/adicional-cartao-de-amor-8-com-foto-personalizada", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399970282/mockup-cart-es-5-xzpqinlraf.png", id: "399970282" },
-        { nome: "Cartão Aniversário 3 + Foto", url: "https://www.floriculturadias.com/adicional-cartao-de-aniversario-3-com-foto-personalizada", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399969157/mockup-cart-es-9-9trvcmg7bv.png", id: "399969157" },
-        { nome: "Cartão de Amor 5 + Foto", url: "https://www.floriculturadias.com/adicional-cartao-de-amor-5-com-foto-personalizada", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399970207/mockup-cart-es-1-ghfzyavt6y.png", id: "399970207" },
-        { nome: "Cartão Amizade 2 + Foto", url: "https://www.floriculturadias.com/adicional-cartao-amizade-2-com-foto-personalizada", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399970315/mockup-cart-es-7f5s1r4444.png", id: "399970315" }
+        { nome: "Cartão de Aniversário 11", preco: "10,00", url: "https://www.floriculturadias.com/adicional-cartao-de-aniversario-11", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399969300/mockup-cart-es-3-d0hxhh4kbf.png" },
+        { nome: "Cartão de Amor 8", preco: "10,00", url: "https://www.floriculturadias.com/adicional-cartao-de-amor-8-com-foto-personalizada", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399970282/mockup-cart-es-5-xzpqinlraf.png" },
+        { nome: "Cartão Aniversário 3 + Foto", preco: "12,00", url: "https://www.floriculturadias.com/adicional-cartao-de-aniversario-3-com-foto-personalizada", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399969157/mockup-cart-es-9-9trvcmg7bv.png" },
+        { nome: "Cartão de Amor 5 + Foto", preco: "12,00", url: "https://www.floriculturadias.com/adicional-cartao-de-amor-5-com-foto-personalizada", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399970207/mockup-cart-es-1-ghfzyavt6y.png" },
+        { nome: "Cartão Amizade 2 + Foto", preco: "12,00", url: "https://www.floriculturadias.com/adicional-cartao-amizade-2-com-foto-personalizada", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399970315/mockup-cart-es-7f5s1r4444.png" }
       ]
     },
     {
       id: "fotos",
       label: "Fotos",
       produtos: [
-        { nome: "Polaroid 9x13cm", url: "https://www.floriculturadias.com/adicional-foto-estilo-polaroid-9x13cm", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399834007/c-pia-de-remo-o-de-fundo-2-rw7kjklee5.png", id: "399834007" },
-        { nome: "Foto Personalizada 10x15", url: "https://www.floriculturadias.com/adicional-foto-personalizada-10x15", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399832787/c-pia-de-remo-o-de-fundo-5-csa76kphfc.png", id: "399832787" },
-        { nome: "Polaroid 7,5x10cm (2 un.)", url: "https://www.floriculturadias.com/adicional-duas-fotos-estilo-polaroid-75x10cm", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399770557/c-pia-de-remo-o-de-fundo-3-1lam27stm2.png", id: "399770557" },
-        { nome: "Porta-Retrato + Foto 10x15", url: "https://www.floriculturadias.com/adicional-porta-retrato-10x15-com-foto-personalizada", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/353041395/porta-retrato-kjlzw1y5jh.png", id: "353041395" }
+        { nome: "Polaroid 9x13cm", preco: "9,00", url: "https://www.floriculturadias.com/adicional-foto-estilo-polaroid-9x13cm", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399834007/c-pia-de-remo-o-de-fundo-2-rw7kjklee5.png" },
+        { nome: "Foto Personalizada 10x15", preco: "10,00", url: "https://www.floriculturadias.com/adicional-foto-personalizada-10x15", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399832787/c-pia-de-remo-o-de-fundo-5-csa76kphfc.png" },
+        { nome: "Polaroid 7,5x10cm (2 un.)", preco: "15,00", url: "https://www.floriculturadias.com/adicional-duas-fotos-estilo-polaroid-75x10cm", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/399770557/c-pia-de-remo-o-de-fundo-3-1lam27stm2.png" },
+        { nome: "Porta-Retrato + Foto 10x15", preco: "35,00", url: "https://www.floriculturadias.com/adicional-porta-retrato-10x15-com-foto-personalizada", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/353041395/porta-retrato-kjlzw1y5jh.png" }
       ]
     },
     {
       id: "pelucias",
       label: "Pelúcias",
       produtos: [
-        { nome: "Almofada Emoji 28cm", url: "https://www.floriculturadias.com/almofada-emoji-28cm-olhos-apaixonados", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/209580347/olhos1-phicpm.jpg", id: "209580347" },
-        { nome: "Urso com Laço Médio", url: "https://www.floriculturadias.com/adicional-urso-de-pelucia-articulado-com-laco-medio", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/278799398/urso-la-o-m-dio-a-631m2v0fkd.png", id: "278799398" },
-        { nome: "Urso com Laço Grande", url: "https://www.floriculturadias.com/adicional-urso-de-pelucia-articulado-com-laco-grande", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/278445583/urso-grande-doce-de-leite-a-j6c945cigv.png", id: "278445583" }
+        { nome: "Almofada Emoji 28cm", preco: "35,00", url: "https://www.floriculturadias.com/almofada-emoji-28cm-olhos-apaixonados", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/209580347/olhos1-phicpm.jpg" },
+        { nome: "Urso com Laço Médio", preco: "55,00", url: "https://www.floriculturadias.com/adicional-urso-de-pelucia-articulado-com-laco-medio", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/278799398/urso-la-o-m-dio-a-631m2v0fkd.png" },
+        { nome: "Urso com Laço Grande", preco: "65,00", url: "https://www.floriculturadias.com/adicional-urso-de-pelucia-articulado-com-laco-grande", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/278445583/urso-grande-doce-de-leite-a-j6c945cigv.png" }
       ]
     },
     {
       id: "canecas",
       label: "Canecas",
       produtos: [
-        { nome: "Caneca Pessoas Incríveis", url: "https://www.floriculturadias.com/adicional-caneca-de-porcelana-310ml-pessoas-incriveis", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/301618819/design-sem-nome-18-vilgs7w44l.png", id: "301618819" },
-        { nome: "Caneca Te Amo Hoje e Sempre", url: "https://www.floriculturadias.com/adicional-caneca-de-porcelana-310ml-te-amo-hoje-e-sempre", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/377093955/design-sem-nome-22-3o9wyiksih.png", id: "377093955" },
-        { nome: "Caneca Parabéns!", url: "https://www.floriculturadias.com/caneca-de-porcelana-310ml-parabens", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/206620740/picwish-17-fikcks.jpg", id: "206620740" },
-        { nome: "Caneca Café e Gratidão", url: "https://www.floriculturadias.com/caneca-de-porcelana-310ml-cafe-e-gratidao", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/210426058/design-sem-nome--12--fzy0f3at2e.png", id: "210426058" },
-        { nome: "Caneca Mundo de Chocolate", url: "https://www.floriculturadias.com/caneca-de-porcelana-310ml-mundo-de-chocolate", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/206098079/design-sem-nome-17-qg4jiblflm.png", id: "206098079" }
+        { nome: "Caneca Pessoas Incríveis", preco: "35,00", url: "https://www.floriculturadias.com/adicional-caneca-de-porcelana-310ml-pessoas-incriveis", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/301618819/design-sem-nome-18-vilgs7w44l.png" },
+        { nome: "Caneca Te Amo Hoje e Sempre", preco: "35,00", url: "https://www.floriculturadias.com/adicional-caneca-de-porcelana-310ml-te-amo-hoje-e-sempre", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/377093955/design-sem-nome-22-3o9wyiksih.png" },
+        { nome: "Caneca Parabéns!", preco: "35,00", url: "https://www.floriculturadias.com/caneca-de-porcelana-310ml-parabens", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/206620740/picwish-17-fikcks.jpg" },
+        { nome: "Caneca Café e Gratidão", preco: "35,00", url: "https://www.floriculturadias.com/caneca-de-porcelana-310ml-cafe-e-gratidao", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/210426058/design-sem-nome--12--fzy0f3at2e.png" },
+        { nome: "Caneca Mundo de Chocolate", preco: "35,00", url: "https://www.floriculturadias.com/caneca-de-porcelana-310ml-mundo-de-chocolate", img: "https://cdn.awsli.com.br/1000x1000/2498/2498336/produto/206098079/design-sem-nome-17-qg4jiblflm.png" }
       ]
     }
   ];
@@ -109,7 +109,7 @@
     var s = document.createElement("style");
     s.id = "fd-adicionais-css";
     s.innerHTML = [
-      ".fd-tag{position:absolute;top:8px;left:8px;z-index:10;font-size:10px;font-weight:600;color:#fff;padding:2px 7px;border-radius:4px;cursor:default;white-space:nowrap;line-height:1.4;}",
+      ".fd-tag{position:absolute;top:8px;left:8px;z-index:10;font-size:9px;font-weight:600;color:#fff;padding:2px 6px;border-radius:3px;cursor:default;white-space:nowrap;line-height:1.4;}",
       ".fd-tag-wrap{position:relative;display:inline-block;}",
       ".fd-tooltip{display:none;position:absolute;top:calc(100% + 5px);left:0;background:#222;color:#fff;font-size:11px;padding:5px 10px;border-radius:4px;white-space:nowrap;z-index:100;pointer-events:none;line-height:1.4;}",
       ".fd-tag-wrap:hover .fd-tooltip{display:block;}",
@@ -125,8 +125,10 @@
       "#fd-complementos .fd-tab.ativo{color:#a91537;border-bottom-color:#a91537;font-weight:600;}",
       "#fd-complementos .fd-tab:hover:not(.ativo){color:#a91537;}",
       "#fd-complementos .fd-panel{display:none;}",
-      "#fd-complementos .fd-panel.ativo{display:grid;grid-template-columns:repeat(auto-fill,minmax(120px,1fr));gap:10px;}",
-      "#fd-complementos .fd-card{border:1px solid #e8e8e8;border-radius:6px;overflow:hidden;text-decoration:none;display:block;transition:border-color .15s,box-shadow .15s;}",
+      "#fd-complementos .fd-panel.ativo{display:block;}",
+      "#fd-complementos .fd-cards-scroll{display:flex;gap:10px;overflow-x:auto;padding-bottom:8px;-webkit-overflow-scrolling:touch;scrollbar-width:none;}",
+      "#fd-complementos .fd-cards-scroll::-webkit-scrollbar{display:none;}",
+      "#fd-complementos .fd-card{border:1px solid #e8e8e8;border-radius:6px;overflow:hidden;text-decoration:none;display:block;flex:0 0 140px;transition:border-color .15s,box-shadow .15s;}",
       "#fd-complementos .fd-card:hover{border-color:#a91537;box-shadow:0 2px 8px rgba(169,21,55,.12);}",
       "#fd-complementos .fd-card img{width:100%;aspect-ratio:1/1;object-fit:cover;display:block;}",
       "#fd-complementos .fd-card-info{padding:7px 8px;}",
@@ -212,24 +214,6 @@
   // ─────────────────────────────────────────────
   // MÓDULO 3 — SEÇÃO DE COMPLEMENTOS (página produto)
   // ─────────────────────────────────────────────
-  function buscarPreco(id, el) {
-    fetch("https://www.floriculturadias.com/api/v1/products/" + id + "/")
-      .then(function (r) { return r.json(); })
-      .then(function (d) {
-        var preco = d && d.price ? parseFloat(d.price) : null;
-        if (!preco && d && d.variations && d.variations.length) {
-          preco = parseFloat(d.variations[0].price);
-        }
-        if (preco && preco > 0) {
-          el.textContent = "R$ " + preco.toFixed(2).replace(".", ",");
-          el.classList.remove("fd-carregando");
-        } else {
-          el.textContent = "";
-        }
-      })
-      .catch(function () { el.textContent = ""; });
-  }
-
   function construirComplementos() {
     // Detecta página de produto pelo seletor real do tema
     var isProduto = !!(document.querySelector(".abas-custom") || document.querySelector("div#descricao"));
@@ -250,8 +234,9 @@
 
     var tabsEl = document.createElement("div");
     tabsEl.className = "fd-tabs";
+    secao.appendChild(tabsEl); // tabs entram na secao ANTES dos painéis
 
-    var paineis = [];
+    var panelEls = []; // coleta painéis separadamente para inserir depois
 
     TABS.forEach(function (tab, idx) {
       // Tab button
@@ -267,10 +252,13 @@
       });
       tabsEl.appendChild(btn);
 
-      // Painel
+      // Painel com scroll horizontal
       var panel = document.createElement("div");
       panel.className = "fd-panel" + (idx === 0 ? " ativo" : "");
       panel.id = "fd-panel-" + tab.id;
+
+      var scroll = document.createElement("div");
+      scroll.className = "fd-cards-scroll";
 
       tab.produtos.forEach(function (prod) {
         var card = document.createElement("a");
@@ -290,29 +278,24 @@
         nome.textContent = prod.nome;
 
         var preco = document.createElement("div");
-        preco.className = "fd-card-preco fd-carregando";
-        preco.textContent = "carregando...";
-        preco.id = "fd-preco-" + prod.id;
+        preco.className = "fd-card-preco";
+        preco.textContent = "R$ " + prod.preco;
 
         info.appendChild(nome);
         info.appendChild(preco);
         card.appendChild(img);
         card.appendChild(info);
-        panel.appendChild(card);
-
-        paineis.push({ id: prod.id, el: preco });
+        scroll.appendChild(card);
       });
 
-      secao.appendChild(tabsEl);
-      secao.appendChild(panel);
+      panel.appendChild(scroll);
+      panelEls.push(panel);
     });
+
+    // Adiciona todos os painéis APÓS as tabs
+    panelEls.forEach(function (p) { secao.appendChild(p); });
 
     ancora.insertAdjacentElement("afterend", secao);
-
-    // Busca preços via API (todos de uma vez, com pequeno delay entre eles)
-    paineis.forEach(function (p, i) {
-      setTimeout(function () { buscarPreco(p.id, p.el); }, i * 120);
-    });
   }
 
   // ─────────────────────────────────────────────
